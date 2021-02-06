9cc9248b3786d63f204c97393c255cdf5358769e
Lucas Monteiro <lucas.monteiro58@gmail.com>
2021-02-06 10:55:28 -0300

    adicionando questão

--- src/components/CardQuestion.vue
+++ src/components/CardQuestion.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="card-question">
-    <div class="title-section">
+    <!-- <div class="title-section">
       <div class="container">
         <div class="title">Congresso de Viena</div>
         <div class="subtitle">- História -</div>
@@ -10,24 +10,90 @@
         src="../assets/images/icons/seta-baixo.png"
         alt="baixo"
       />
+    </div> -->
+    <div class="question-section">
+      <div class="enunciado-texto">
+        {{ question.enunciado.texto }}
+      </div>
+      <div class="enunciado-fonte">{{ question.enunciado.fonte }}</div>
+      <div class="enunciado-acesso">{{ question.enunciado.acesso }}</div>
+      <div class="imagem">
+        <img
+          :src="question.imagem.link"
+          :alt="question.imagem.title"
+          @click="show"
+        />
+        <div class="imagem-font">
+          {{ question.imagem.fonte }} <span> {{ question.imagem.acesso }}</span>
+        </div>
+      </div>
+      <div class="quention-pergunta">{{ question.pergunta }}</div>
+      <div class="question-alternativas">
+        <div class="alternativa-a">{{ question.alternativas.a.text }}</div>
+        <div class="alternativa-b">{{ question.alternativas.b.text }}</div>
+        <div class="alternativa-c">{{ question.alternativas.c.text }}</div>
+        <div class="alternativa-d">{{ question.alternativas.d.text }}</div>
+      </div>
     </div>
-    <!-- <div class="question-section"></div>
-    <div class="video-section"></div> -->
+    <!-- <div class="video-section"></div> -->
+    <viewer :images="images" @inited="inited" class="viewer" ref="viewer">
+      <img v-for="src in images" :src="src" :key="src" class="none" />
+    </viewer>
   </section>
 </template>
 
 <script>
+import { question } from "../consts/index";
+
+import "viewerjs/src/css/viewer.scss";
+import Viewer from "v-viewer";
+import Vue from "vue";
+
+Vue.use(Viewer);
+
+Viewer.setDefaults({
+  button: true,
+  navbar: false,
+  title: false,
+  toolbar: false,
+  tooltip: false,
+  movable: false,
+  zoomable: true,
+  rotatable: false,
+  scalable: false,
+  fullscreen: false,
+  keyboard: false
+});
+
 export default {
-  name: "CardQuestion"
+  name: "CardQuestion",
+  data() {
+    return {
+      question,
+      images: [question.imagem.link]
+    };
+  },
+  methods: {
+    inited(viewer) {
+      this.$viewer = viewer;
+    },
+    show() {
+      this.$viewer.show();
+    }
+  }
 };
 </script>
 <style lang="scss" scoped>
+.none {
+  display: none;
+}
 .card-question {
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color: white;
+  z-index: 10;
   .title-section {
+    background-color: white;
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -67,5 +133,94 @@ export default {
       }
     }
   }
+
+  .question-section {
+    display: flex;
+    flex-direction: column;
+    width: 70vw;
+    height: 65vh;
+    box-shadow: 1px 0px 6px 0px rgba(158, 158, 158, 1);
+    border-radius: 3vw;
+    background-color: white;
+    padding: 80px 150px;
+    font-size: 25px;
+    overflow: auto;
+
+    /* width */
+
+    .enunciado-texto {
+      text-align: justify;
+    }
+
+    .enunciado-fonte {
+      margin-top: 30px;
+      font-size: 18px;
+      font-style: italic;
+    }
+
+    .enunciado-acesso {
+      font-size: 18px;
+    }
+
+    .imagem {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      margin-top: 70px;
+
+      img {
+        max-height: 350px;
+      }
+
+      .imagem-font {
+        font-size: 16px;
+        font-style: italic;
+        margin-top: 10px;
+
+        span {
+          font-style: normal;
+        }
+      }
+    }
+
+    .quention-pergunta {
+      margin: 0px 30px;
+      margin-top: 70px;
+      text-align: justify;
+    }
+
+    .question-alternativas {
+      div {
+        text-align: justify;
+        margin: 30px 0;
+        padding: 20px 30px;
+        border: 2px solid #4a462ac2;
+        border-radius: 1.5vw;
+      }
+    }
+  }
+}
+
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #dbdbdb;
+  border-radius: 20px;
+  margin-bottom: 42px;
+  margin-top: 42px;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888;
+  border-radius: 20px;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
 }
 </style>
---
+++ src/consts/index.js
@@ -0,0 +1,40 @@
+export const question = {
+  enunciado: {
+    texto:
+      '(UEMG) "Há duzentos anos, em 9 de junho de 1815, encerrava-se o Congresso de Viena, conferência de países europeus que, após nove meses de deliberações, estabeleceu um plano de paz de longo prazo para o continente, que vivia um contexto político conturbado (...). Para alcançar esse objetivo, os diplomatas presentes ao Congresso de Viena criaram um mecanismo de pesos e contrapesos conhecido como "Concerto Europeu" (...). O Concerto Europeu procurou substituir um arranjo unipolar por um sistema inovador de consultas plurilaterais. Esse esforço visava a garantir a estabilidade europeia no pós-guerra".',
+    fonte:
+      "http://blog.itamaraty.gov.br/63-historia/146-200-anos-do-congresso-de-viena.",
+    acesso: "Acesso em: 20/7/2015."
+  },
+
+  imagem: {
+    link: require("../assets/images/question-image.png"),
+    fonte: "http://blog.itamaraty.gov.br/images/viena.jpg. ",
+    acesso: "  Acesso em: 19/9/2015.",
+    title: "imagem"
+  },
+  pergunta:
+    "O contexto conturbado vivido pela Europa antes do Congresso de Viena e os resultados deste foram, respectivamente:  ",
+  alternativas: {
+    a: {
+      text:
+        "a) A guerra dos sete anos que colocaram em confronto Inglaterra e França em função de disputas territoriais na América. – A expulsão da França da Liga das Nações por ter desrespeitado regras internacionais preestabelecidas. ",
+      correct: false
+    },
+    b: {
+      text:
+        "b) A disputa imperialista protagonizada pelas nações europeias em função da crise econômica vivida no século XIX. – Evitou-se provisoriamente um conflito de proporções mundiais já que, por meio de concessões, garantiu-se um equilíbrio político. ",
+      correct: false
+    },
+    c: {
+      text:
+        "c) A expansão napoleônica que destronou reis e promoveu a invasão e ocupação militar sobre diversas regiões. – Restauração das monarquias depostas por Napoleão, legitimação das existentes à época e a criação da Santa Aliança. ",
+      correct: false
+    },
+    d: {
+      text:
+        "d) A primeira grande guerra, que foi consequência de um momento marcado pelo nacionalismo exacerbado e por rivalidades econômicas e territoriais. – A imposição de uma paz despreocupada com o equilíbrio mundial pois humilhava os derrotados.",
+      correct: false
+    }
+  }
+};
