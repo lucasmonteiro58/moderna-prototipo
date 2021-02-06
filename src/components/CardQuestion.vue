9d986a1685a7d822a235ca986eb4f79ea234cd47
Lucas Monteiro <lucas.monteiro58@gmail.com>
2021-02-05 21:07:28 -0300

    tela inicial

---
+++ src/components/CardQuestion.vue
@@ -0,0 +1,71 @@
+<template>
+  <section class="card-question">
+    <div class="title-section">
+      <div class="container">
+        <div class="title">Congresso de Viena</div>
+        <div class="subtitle">- História -</div>
+      </div>
+      <img
+        class="seta-baixo"
+        src="../assets/images/icons/seta-baixo.png"
+        alt="baixo"
+      />
+    </div>
+    <!-- <div class="question-section"></div>
+    <div class="video-section"></div> -->
+  </section>
+</template>
+
+<script>
+export default {
+  name: "CardQuestion"
+};
+</script>
+<style lang="scss" scoped>
+.card-question {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: white;
+  .title-section {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 40vw;
+    height: 380px;
+    border: 8px solid #4a462a;
+    border-radius: 3vw;
+
+    .container {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      height: 250px;
+      border-bottom: 8px solid #4a462a;
+      width: 100%;
+
+      .title {
+        font-family: Maven-Black;
+        font-size: 64px;
+        text-align: center;
+      }
+
+      .subtitle {
+        font-size: 48px;
+        text-align: center;
+      }
+    }
+
+    .seta-baixo {
+      width: 120px;
+      margin-top: 35px;
+      cursor: pointer;
+
+      &:hover {
+        margin-top: 38px;
+      }
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,93 @@
 <template>
   <div class="home">
     <Slide noOverlay>
-      <a id="home" href="#">
-        <span>Home</span>
-      </a>
+      <img class="logo-menu" src="../assets/images/logo-moderna.png" />
+      <main id="page-wrap">
+        <div>
+          <img src="../assets/images/icons/reiniciar.png" />
+          <span>Reiniciar</span>
+        </div>
+        <div>
+          <img src="../assets/images/icons/som-on.png" />
+          <span>Som Ativado</span>
+        </div>
+        <div>
+          <img src="../assets/images/icons/acessibilidade.png" />
+          <span>Acessibilidade</span>
+        </div>
+        <div>
+          <img src="../assets/images/icons/ajuda.png" />
+          <span>Ajuda</span>
+        </div>
+        <div>
+          <img src="../assets/images/icons/creditos.png" />
+          <span>Créditos</span>
+        </div>
+        <div>
+          <img src="../assets/images/icons/compartilhar.png" />
+          <span>Compartilhar</span>
+        </div>
+      </main>
     </Slide>
+    <img
+      class="logo-page"
+      src="../assets/images/logo-moderna-white.png"
+      alt="logo"
+    />
+    <div class="center-content">
+      <CardQuestion></CardQuestion>
+    </div>
   </div>
 </template>
 
 <script>
 import { Slide } from "vue-burger-menu";
+import CardQuestion from "../components/CardQuestion";
 export default {
   name: "Home",
-  components: { Slide }
+  components: { Slide, CardQuestion }
 };
 </script>
-<style scoped></style>
+<style lang="scss" scoped>
+.center-content {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+.logo-page {
+  position: absolute;
+  top: 16px;
+  left: 60px;
+  width: 12%;
+}
+.logo-menu {
+  transform: scale(0.8);
+  margin-top: 18px;
+  margin-left: 8px;
+}
+#page-wrap {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-top: 50px;
+
+  div {
+    font-size: 22px;
+    letter-spacing: 1px;
+    padding: 20px 30px;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    width: 300px;
+
+    &:hover {
+      background-color: #e7e7e7;
+    }
+
+    img {
+      margin-right: 20px;
+    }
+  }
+}
+</style>
